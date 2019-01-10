b52f257db80afe11a31ee61200e44e8d1010c4d1
Idhrendur <idhrendur@hotmail.com>
2019-01-09 19:44:53 -0800

    Restore promythyus' work for a checkbox multi-select option type (#15)
    * Restore promythyus' work in allowing a checkbox multi-select option
    type.
    
    * Remove some commented-out lines.

---
+++ Frontend.Core/Model/PreferenceEntries/Interfaces/IStringListPreferenceEntry.cs
@@ -0,0 +1,8 @@
+using Frontend.Core.Model.Preferences.Interfaces;
+
+namespace Frontend.Core.Model.PreferenceEntries.Interfaces
+{
+	public interface IStringListPreferenceEntry : IPreferenceEntry<string, IStringListPreference>
+	{
+	}
+}
