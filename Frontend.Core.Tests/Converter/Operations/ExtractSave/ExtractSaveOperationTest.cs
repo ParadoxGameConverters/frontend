20652a4561890f024c85c1198560cfcdaf965067
Idhrendur <idhrendur@hotmail.com>
2019-01-09 18:49:54 -0800

    Codacy fixes (#14)
    * Address the security fixes.
    
    * Make the default case throw an exception.
    
    * Remove unused code.
    
    * Remove some more dead code.
    
    * Remove more unused code.
    
    * Remove even more dead code.
    
    * Clean up extract save operation test.

--- Frontend.Core.Tests/Converter/Operations/ExtractSave/ExtractSaveOperationTest.cs
+++ Frontend.Core.Tests/Converter/Operations/ExtractSave/ExtractSaveOperationTest.cs
@@ -8,77 +8,72 @@ using Moq;
 
 namespace Frontend.Core.Tests.Converter.Operations.ExtractSave
 {
-    [TestClass]
-    public class ExtractSaveOperationTest : UnitTestBase
-    {
-        private Mock<IFileProxy> fileProxy;
-        private Mock<IFolderProxy> folderProxy;
-        private Mock<ICompressedSaveChecker> mockCompressedSaveChecker;
-        private Mock<IConverterOptions> mockConverterOptions;
-        private Mock<IConverterSettings> mockCurrentConverter;
-        private Mock<IDirectoryHelper> mockDirectoryHelper;
-        private Mock<IEnvironmentProxy> mockEnvironment;
-        private Mock<IMessageBoxProxy> mockMessageBoxProxy;
-        private Mock<IRequiredFile> mockSavegame;
-        private ExtractSaveOperation testTarget;
-        private Mock<IZipFileHelper> zipFileHelper;
+	[TestClass]
+	public class ExtractSaveOperationTest : UnitTestBase
+	{
+		private Mock<IFolderProxy> folderProxy;
+		private Mock<ICompressedSaveChecker> mockCompressedSaveChecker;
+		private Mock<IConverterOptions> mockConverterOptions;
+		private Mock<IDirectoryHelper> mockDirectoryHelper;
+		private Mock<IEnvironmentProxy> mockEnvironment;
+		private Mock<IMessageBoxProxy> mockMessageBoxProxy;
+		private Mock<IRequiredFile> mockSavegame;
+		private ExtractSaveOperation testTarget;
+		private Mock<IZipFileHelper> zipFileHelper;
 
-        protected override void OnCleanup()
-        {
-            mockSavegame = null;
-            mockCurrentConverter = null;
-            testTarget = null;
-            mockMessageBoxProxy = null;
-            mockDirectoryHelper = null;
-            mockConverterOptions = null;
-            mockCompressedSaveChecker = null;
-            fileProxy = null;
-            zipFileHelper = null;
-            mockEnvironment = null;
-            base.OnCleanup();
-        }
+		protected override void OnCleanup()
+		{
+			mockSavegame = null;
+			testTarget = null;
+			mockMessageBoxProxy = null;
+			mockDirectoryHelper = null;
+			mockConverterOptions = null;
+			mockCompressedSaveChecker = null;
+			zipFileHelper = null;
+			mockEnvironment = null;
+			base.OnCleanup();
+		}
 
-        protected override void OnInitialize()
-        {
-            base.OnInitialize();
-            mockCompressedSaveChecker = new Mock<ICompressedSaveChecker>();
-            mockConverterOptions = new Mock<IConverterOptions>();
-            mockDirectoryHelper = new Mock<IDirectoryHelper>();
-            mockMessageBoxProxy = new Mock<IMessageBoxProxy>();
-            fileProxy = new Mock<IFileProxy>();
-            folderProxy = new Mock<IFolderProxy>();
-            zipFileHelper = new Mock<IZipFileHelper>();
-            mockEnvironment = new Mock<IEnvironmentProxy>();
+		protected override void OnInitialize()
+		{
+			base.OnInitialize();
+			mockCompressedSaveChecker = new Mock<ICompressedSaveChecker>();
+			Mock<IConverterSettings> mockCurrentConverter = new Mock<IConverterSettings>();
+			mockConverterOptions = new Mock<IConverterOptions>();
+			mockDirectoryHelper = new Mock<IDirectoryHelper>();
+			mockMessageBoxProxy = new Mock<IMessageBoxProxy>();
+			Mock<IFileProxy> fileProxy = new Mock<IFileProxy>();
+			folderProxy = new Mock<IFolderProxy>();
+			zipFileHelper = new Mock<IZipFileHelper>();
+			mockEnvironment = new Mock<IEnvironmentProxy>();
+			mockSavegame = new Mock<IRequiredFile>();
 
-            mockCurrentConverter = new Mock<IConverterSettings>();
-            mockConverterOptions.Setup(o => o.CurrentConverter).Returns(mockCurrentConverter.Object);
+			mockConverterOptions.Setup(o => o.CurrentConverter).Returns(mockCurrentConverter.Object);
+			mockCurrentConverter.Setup(c => c.AbsoluteSourceSaveGame).Returns(mockSavegame.Object);
 
-            mockSavegame = new Mock<IRequiredFile>();
-            mockCurrentConverter.Setup(c => c.AbsoluteSourceSaveGame).Returns(mockSavegame.Object);
+			testTarget = new ExtractSaveOperation(
+				 mockConverterOptions.Object,
+				 mockCompressedSaveChecker.Object,
+				 zipFileHelper.Object,
+				 mockEnvironment.Object,
+				 fileProxy.Object,
+				 folderProxy.Object);
+		}
 
-            testTarget = new ExtractSaveOperation(
-                mockConverterOptions.Object,
-                mockCompressedSaveChecker.Object,
-                zipFileHelper.Object,
-                mockEnvironment.Object,
-                fileProxy.Object,
-                folderProxy.Object);
-        }
+		[TestMethod]
+		public void CanRun_ValidAbsoluteSavePathTest()
+		{
+			var testPath = "x:\\savegame.eu4";
+			mockSavegame.Setup(s => s.SelectedValue).Returns(testPath);
+			mockCompressedSaveChecker.Setup(sc => sc.IsCompressedSave(It.Is<string>(path => path.Equals(testPath))))
+				 .Returns(true);
+			Assert.IsTrue(testTarget.CanRun());
+		}
 
-        [TestMethod]
-        public void CanRun_ValidAbsoluteSavePathTest()
-        {
-            var testPath = "x:\\savegame.eu4";
-            mockSavegame.Setup(s => s.SelectedValue).Returns(testPath);
-            mockCompressedSaveChecker.Setup(sc => sc.IsCompressedSave(It.Is<string>(path => path.Equals(testPath))))
-                .Returns(true);
-            Assert.IsTrue(testTarget.CanRun());
-        }
-
-        [TestMethod]
-        public void CanRun_InvalidAbsoluteSavePathTest()
-        {
-            Assert.IsFalse(testTarget.CanRun());
-        }
-    }
+		[TestMethod]
+		public void CanRun_InvalidAbsoluteSavePathTest()
+		{
+			Assert.IsFalse(testTarget.CanRun());
+		}
+	}
 }
